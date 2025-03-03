6d34f679bf9d79344211f873b05530c14a579648
rdlhio <contact@rdlh.io>
2025-03-03 19:47:09 +0100

    Fix tailwind

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/main.css'  // Make sure this path matches your CSS file location
-
-createApp(App).mount('#app') 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/assets/*.css"  // Make sure this includes your CSS files
   ],
   darkMode: 'class',
   theme: {
